5f670a86a2f5e33aee40ff6b6e28aaf46668b20d
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-13 16:15:52 +0200

    Write fetching method to get movies

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,28 @@ import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      movies: [],
+      title: '',
+      year: ''
+    }
+  },
+  mounted(){
+
+  },
+  methods: {
+    async getMovies(){
+      if(!this.title){
+        alert('Please enter the title!');
+        return;
+      }
+
+      const movies = await fetch(`https://www.omdbapi.com/?${this.year > 0 ? `y=${this.year}&` : ''}s=${this.title}&apiKey=e6047284`);
+      const moviesResponse = await movies.json();
+      console.log(moviesResponse);
+    }
+  },
   components: {
     Navbar: Navbar
   }
