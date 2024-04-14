8b36e74b9df21d69e103eb85ec887336a0857a03
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-14 16:27:23 +0200

    Rerun getSearchedMovies function when the url changes

--- src/pages/SearchedMovies.vue
+++ src/pages/SearchedMovies.vue
@@ -1,9 +1,43 @@
 <template>
-    <h1>Searched movies</h1>
+    <div class="container">
+        <div v-if="error" class="alert alert-danger" role="alert">{{error}}</div>
+        <template v-else>
+            <h1 class="justify-content-lg-center">Searched movies: {{this.$route.query.title}}{{ checkIfYearExists }}</h1>
+        </template>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'SearchedMovies',
+    data(){
+        return{
+            movies: [],
+            error: ''
+        }
+    },
+    mounted(){
+        this.getSearchedMovies();
+    },
+    methods: {
+        getSearchedMovies(){
+            const query = this.$route.query;
+            if(!query.title){
+                this.error = 'Please enter a movie title!';
+                return;
+            }
+        }
+    },
+    watch: {
+        '$route'(){
+            this.getSearchedMovies();
+        }
+    },
+    computed: {
+        checkIfYearExists(){
+            return this.$route.query.year ? ` | ${this.$route.query.year}` : '';
+        }
+    }
+
 }
 </script>
