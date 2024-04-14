ef46622acc56a233770de93ebdca5d5a2c11d723
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-14 16:45:31 +0200

    Fetch movie api and save data

--- src/pages/SearchedMovies.vue
+++ src/pages/SearchedMovies.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="container">
         <div v-if="error" class="alert alert-danger" role="alert">{{error}}</div>
-        <template v-else>
+        <template v-else-if="movies.length">
             <h1 class="justify-content-lg-center">Searched movies: {{this.$route.query.title}}{{ checkIfYearExists }}</h1>
         </template>
     </div>
 </template>
 
 <script>
+const apiKey = import.meta.env.VITE_API_KEY;
+
 export default {
     name: 'SearchedMovies',
     data(){
@@ -20,11 +22,22 @@ export default {
         this.getSearchedMovies();
     },
     methods: {
-        getSearchedMovies(){
+        async getSearchedMovies(){
             const query = this.$route.query;
             if(!query.title){
                 this.error = 'Please enter a movie title!';
                 return;
+            }
+
+            
+            const moviesData = await fetch(`https://www.omdbapi.com/?${this.year > 0 ? `y=${this.year}&` : ''}s=${this.title}&apiKey=${apiKey}`);
+            const moviesResponse = await moviesData.json();
+
+            if(moviesData.ok){
+                this.movies = moviesResponse.Search;
+                this.error = '';
+            } else{
+                this.error = moviesResponse.Error;
             }
         }
     },
