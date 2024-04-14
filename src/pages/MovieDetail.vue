a1c91c85cf5a5aeb5a8686b2433542d0572bc117
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-14 20:41:56 +0200

    Get movie details by imdbId

--- src/pages/MovieDetail.vue
+++ src/pages/MovieDetail.vue
@@ -1,9 +1,46 @@
 <template>
-    <h1>Movie Detail</h1>
+    <div v-if="error" class="alert alert-danger" role="alert">{{error}}</div>
+    <template v-else>
+        <h1>Movie Detail</h1>
+
+    </template>
 </template>
 
 <script>
+const apiKey = import.meta.env.VITE_API_KEY;
+
 export default {
     name: 'MovieDetail',
+    data(){
+        return {
+            movieDetail: {},
+            error: ''
+        }
+    },
+    mounted(){
+        this.getMovieDetails();
+    },
+    methods: {
+        async getMovieDetails(){
+            const movieId = this.$route.params.id;
+            
+            if(!movieId){
+                this.error = 'Invalid url parameter!';
+                return;
+            }
+
+            const movieDetail = await fetch(`https://omdbapi.com/?apikey=${apiKey}&i=${movieId}`);
+            const movieDetailResponse = await movieDetail.json();
+            const response = movieDetailResponse.Response === 'True';
+
+            if(response){
+                console.log(movieDetailResponse);
+                this.movieDetail = movieDetailResponse;
+                this.error = '';
+            } else{
+                this.error = movieDetailResponse.Error;
+            }
+        }
+    }
 }
 </script>
